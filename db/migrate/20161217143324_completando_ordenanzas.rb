bd6fabad0ec4267eb6d6e98e9bae7834062cc94b
rodley82 <rodolfo.leyes@gmail.com>
2016-12-17 11:51:43 -0300

    Agregadas columnas faltantes en ordenanzas

---
+++ db/migrate/20161217143324_completando_ordenanzas.rb
@@ -0,0 +1,11 @@
+class CompletandoOrdenanzas < ActiveRecord::Migration
+  def change
+    add_column :ordenanzas, :numero, :string
+    add_column :ordenanzas, :anio, :string
+    add_column :ordenanzas, :tema, :string
+    add_column :ordenanzas, :sintesis, :text
+    add_column :ordenanzas, :vigente, :string
+    add_column :ordenanzas, :unidad_id, :string
+    add_column :ordenanzas, :generalidad_id, :string
+  end
+end
